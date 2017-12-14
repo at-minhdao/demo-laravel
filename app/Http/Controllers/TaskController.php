07a063aec8573abd6b0026d93a9f12d564f1c347
Minh Dao T <minh.dao@asiantech.vn>
2017-12-14 10:07:26 +0700

    edit TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\TaskRepository;
 
 class TaskController extends Controller
 {
+    protected $tasks;
+
     public function __construct()
     {
         $this->middleware('auth');
+        $this->tasks = $tasks;
     }
     public function index(Request $request)
     {
-        return view('tasks.index');
+        return view('tasks.index', [
+            'tasks' => $this->tasks->forUser($request->user()),
+        ]);
     }
     public function store(Request $request)
     {
