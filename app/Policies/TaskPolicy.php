ca3937120df9a57eb192a3a1f9ec896149e3180b
Minh Dao T <minh.dao@asiantech.vn>
2017-12-14 10:32:46 +0700

    update TaskPolicy

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\User;
+use App\Task;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class TaskPolicy
@@ -14,8 +15,8 @@ class TaskPolicy
      *
      * @return void
      */
-    public function __construct()
+    public function destroy(User $user, Task $task)
     {
-        //
+        return $user->id === $task->user_id;
     }
 }
