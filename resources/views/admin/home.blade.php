bc52140e11ce78f212815662b521ca9b2dc7c785
Minh Dao T <minh.dao@asiantech.vn>
2017-12-19 16:12:11 +0700

    create view for admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        @if (Session::has('msg'))
+            <p class="alert alert-success">{{ Session::get('msg') }}</p>
+        @endif
+        @if (Session::has('msgErr'))
+            <p class="alert alert-danger">{{ Session::get('msgErr') }}</p>
+        @endif
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard ADMIN</div>
+
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <a href="{{ route('tasks.index') }}">Tasks</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
