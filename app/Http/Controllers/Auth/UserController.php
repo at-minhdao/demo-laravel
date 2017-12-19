a3f8458db4d6a45d53d9c92ae21fd330f5ef06eb
Minh Dao T <minh.dao@asiantech.vn>
2017-12-19 10:53:33 +0700

    change profile user login

--- app/Http/Controllers/Auth/UserController.php
+++ app/Http/Controllers/Auth/UserController.php
@@ -2,10 +2,39 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
-    //
+    public function edit($id)
+    {
+        return view('auth.edit', [
+            'user' => User::findOrFail($id),
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Validate
+        Validator::extend('without_spaces', function($attr, $value){
+            return preg_match('/^\S*$/u', $value);
+        });
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'password' => 'without_spaces|confirmed',
+        ]);
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        if (trim($request->password) == null) {
+            $user->save();
+            return redirect('home')->with('msg', 'Change profile success');
+        } else {
+            $user->password = bcrypt($request->password);
+            $user->save();
+            return redirect('home')->with('msg', 'Change profile success');            
+        }
+    }
 }
